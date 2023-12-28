7ac09ccb2c25cab4be0a6d4aa83f6b9bc2bb53ac
stugl <stu@giglabs.io>
2023-12-28 11:18:54 -0800

    revised output

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route('/extract', methods=['POST'])
 def extract():
     data = request.json
-    # Validate if 'inputurl' is in the data
     if not data or 'inputurl' not in data:
         return jsonify({'error': 'No input URL provided'}), 400
 
@@ -20,8 +19,7 @@ def extract():
         else:
             return jsonify({'error': 'Failed to fetch URL'}), 500
     except Exception as e:
-        # Log the exception for debugging
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run()
